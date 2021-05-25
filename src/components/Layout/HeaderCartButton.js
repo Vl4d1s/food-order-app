110165d06603a0b2cabb649a335a3f0865225ac6
Vladis <vladismarkin@gmail.com>
2021-05-25 21:11:54 +0300

    Adding cart context

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -2,6 +2,7 @@ import React from "react";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
 
+
 const HeaderCartButton = (props) => {
   return (
     <button className={classes.button} onClick={props.onClick}>
