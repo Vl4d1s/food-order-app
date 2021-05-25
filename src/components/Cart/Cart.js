1d2fb9df901b81ba873e768da81604128be2190a
Vladis <vladismarkin@gmail.com>
2021-05-25 20:58:32 +0300

    Managing Cart & Modal State

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -12,14 +12,16 @@ const Cart = (props) => {
   );
 
   return (
-    <Modal>
+    <Modal onClose={props.onClose}>
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
         <span>35.52</span>
       </div>
       <div className={classes.actions}>
-        <button className={classes["button--alt"]}>Cancel</button>
+        <button className={classes["button--alt"]} onClick={props.onClose}>
+          Close
+        </button>
         <button className={classes.button}>Order</button>
       </div>
     </Modal>
